b7683a6a3203042971e31c02239cc3fa2e30bd48
Vincent Fiszbin <fiszbin.vincent@gmail.com>
2022-06-01 10:32:24 +0200

    Move player using wasd keys

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: vfiszbin <vfiszbin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/29 11:45:16 by vfiszbin          #+#    #+#             */
-/*   Updated: 2022/06/01 09:03:10 by vfiszbin         ###   ########.fr       */
+/*   Updated: 2022/06/01 10:29:12 by vfiszbin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,13 +46,53 @@ int	close_program(t_vars *vars)
 	// exit(1); //changer par un mlx_loop_end ?
 	return (0);
 }
+/*
+		 (-1,0)
+			^
+			|
+			|
+(0,-1)<---P(i,j)--->(0,+1)
+			|
+			|
+			ˇ
+		  (+1,0)
+
+*/
+void move_player(int d_i, int d_j, t_vars *vars)
+{
+	int new_i;
+	int new_j;
+
+	new_i = vars->player_i + d_i;
+	new_j = vars->player_j + d_j;
+
+	if (new_i < 0 || new_j < 0 || new_i > (vars->map_height - 1) || new_j > (vars->map_width - 1))
+	{
+		ft_printf("You can't go out of map !\n");
+		return ;
+	}
+	ft_printf("initial case contains : %c\n", vars->map[(vars->player_i * vars->map_width + vars->player_i) + vars->player_j]);
+	vars->map[(vars->player_i * vars->map_width + vars->player_i) + vars->player_j] = '0';
+	mlx_put_image_to_window(vars->mlx, vars->win, vars->ground_img, vars->player_j * 50, vars->player_i * 50);	
+	vars->player_i = new_i;
+	vars->player_j = new_j;
+	vars->map[(new_i * vars->map_width + new_i) + new_j] = 'P';
+	mlx_put_image_to_window(vars->mlx, vars->win, vars->player_img, new_j * 50, new_i * 50);
+}
 
 int	key_hook(int keycode, t_vars *vars)
 {
 	ft_printf("keycode = %d\n", keycode);
 	if (keycode == 65307)
 		close_program(vars);
-	
+	else if (keycode == 119)
+		move_player(-1, 0, vars);
+	else if (keycode == 115)
+		move_player(1, 0, vars);
+	else if (keycode == 97)
+		move_player(0, -1, vars);
+	else if (keycode == 100)
+		move_player(0, 1, vars);
 	return (0);
 }
 
@@ -123,6 +163,8 @@ void load_images(t_vars *vars)
 
 	vars->player_img = mlx_xpm_file_to_image(vars->mlx, "fox.xpm", &img_width, &img_height); //proteger
 	ft_printf("%d %d\n",img_width, img_height);
+	vars->ground_img = mlx_xpm_file_to_image(vars->mlx, "grass.xpm", &img_width, &img_height); //proteger
+
 }
 
 /*Draw the images to the correct coordinates on the window,
@@ -130,27 +172,30 @@ according to their position in the map string
 Access elements of 2D map contained in str : map[(i * map_width + k) + j]
 i : height coordinate
 j : width coordinate
-k : offset to jump "\n" characters*/
+k : offset to jump "\n" characters (==i)*/
 void draw_map(t_vars *vars)
 {
 	int i;
 	int j;
-	int k;
 	
 	i = 0;
 	j = 0;
-	k = 0;
-	while (vars->map[(i * vars->map_width + k) + j])
+	while (vars->map[(i * vars->map_width + i) + j])
 	{
-		// ft_printf("%c", vars->map[(i * vars->map_width + k) + j]);
+		// ft_printf("%c", vars->map[(i * vars->map_width + i) + j]);
 		// ft_printf("i=%d j=%d\n", i, j);
-		// ft_printf("el=%d\n", ((i * vars->map_width + k) + j));
-		if (vars->map[(i * vars->map_width + k) + j] == 'P')
-			mlx_put_image_to_window(vars->mlx, vars->win, vars->player_img, j * 50, i * 50);
+		// ft_printf("el=%d\n", ((i * vars->map_width + i) + j));
+		if (vars->map[(i * vars->map_width + i) + j] == 'P')
+		{
+			vars->player_i = i; //recup coordonnees avant lors de la verification de map ?
+			vars->player_j = j;
+			mlx_put_image_to_window(vars->mlx, vars->win, vars->player_img, j * 50, i * 50);			
+		}
+		else if (vars->map[(i * vars->map_width + i) + j] == '0')
+			mlx_put_image_to_window(vars->mlx, vars->win, vars->ground_img, j * 50, i * 50);
 		j++;
-		if (vars->map[(i * vars->map_width + k) + j] == '\n'){
+		if (vars->map[(i * vars->map_width + i) + j] == '\n'){
 			i++;
-			k++;
 			j = 0;
 		}
 	}
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: vfiszbin <vfiszbin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 18:25:26 by vfiszbin          #+#    #+#             */
-/*   Updated: 2022/05/31 10:05:16 by vfiszbin         ###   ########.fr       */
+/*   Updated: 2022/06/01 10:24:27 by vfiszbin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,14 @@ typedef struct	s_vars {
 	void	*mlx;
 	void	*win;
 	void	*player_img;
+	void	*ground_img;
 	char 	*map;
 	int		map_width;
 	int		map_height;
+	int		player_i;
+	int		player_j;
 }	t_vars;
 
+void draw_map(t_vars *vars);
 
 #endif
